3e4083b4c043649ffc5f163ebf9445d3cd131bf3
sikdermdariff <sikdermohammadarif@gmail.com>
2021-10-25 19:52:27 +0600

    Wallet page tab changes done and basic tasks done

--- inc/js/copyToClipboard.js
+++ inc/js/copyToClipboard.js
@@ -1,12 +1,33 @@
-const getText = document.querySelector('input#ver-token');
-const copyTextBtn = document.getElementById('ver-token-btn1');
-const tickBtn = document.getElementById('ver-token-btn2');
+function reciveMoneyCopyBtn() {
+    const getText = document.querySelector('input#ver-token');
+    const copyTextBtn = document.getElementById('ver-token-btn1');
+    const tickBtn = document.getElementById('ver-token-btn2');
+    copyTextBtn.addEventListener('click', function(){
+        getText.select();
+        document.execCommand('copy');
+        copyTextBtn.style.display = 'none';
+        tickBtn.style.display = 'block';
+    });
+}
+reciveMoneyCopyBtn();
 
-copyTextBtn.addEventListener('click', function(){
+function sendMoneyCopyBtn(){
+    const getText = document.querySelector('input#fisrt-input');
+    const copyTextBtn = document.getElementById('send-money-copy');
+    copyTextBtn.addEventListener('click', function(){
+        getText.select();
+        document.execCommand('copy');
+    });
+}
+sendMoneyCopyBtn();
 
-    getText.select();
-    document.execCommand('copy');
-
-    copyTextBtn.style.display = 'none';
-    tickBtn.style.display = 'block';
-})
+function sendMoneyPasteBtn(){
+    const getText = document.querySelector('input#fisrt-input');
+    const pasteTextBtn = document.getElementById('send-money-paste');
+    pasteTextBtn.addEventListener('click', function(){
+        navigator.clipboard.readText().then(
+            clipboardData => getText.value = clipboardData
+        )
+    });
+}
+sendMoneyPasteBtn();
--- inc/js/wallet.tab.js
+++ inc/js/wallet.tab.js
@@ -1,10 +1,7 @@
 const getTabUl = document.querySelector('#wallet-tab-header');
 const allTabBtn = document.querySelectorAll('#wallet-tab-header > li');
-
 const tabBody = document.querySelector('#wallet-tab-body');
 const tabItem = tabBody.getElementsByClassName('tab-item');
-
-
 for (let i = 0; i < allTabBtn.length; i++) {
     allTabBtn[i].addEventListener('click', ()=> {
         getTabUl.getElementsByClassName('active')[0].classList.remove('active');
